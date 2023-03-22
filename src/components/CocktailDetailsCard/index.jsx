df4aa9da130152f7d6ac4c6bcfd0e74d8e34c115
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-03-22 19:51:29 +0530

    architecture setup

---
+++ src/components/CocktailDetailsCard/index.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material";
+
+const CocktailDetails = () => {
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+
+        }}>
+            {/* Image */}
+            <Box></Box>
+            {/* Details */}
+            <Box>
+
+            </Box>
+        </Box>
+    )
+}
+
+export default CocktailDetails;
