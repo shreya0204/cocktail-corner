0b60fd60823b3824a28bc33614ecbe9417241dc9
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-03-22 19:57:12 +0530

    navbar component added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    API_URL: 'https://www.thecocktaildb.com/api/json/v1/1',
+  },
+
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,19 @@
-export default function Home() {
+import { Box } from "@mui/material"
+import Navbar from "@/components/Navbar"
+// import HeroSection from "@/components/HeroSection"
+
+
+export default function Home(props) {
+  console.log(props)
+  const { categories, randomDrinks } = props
+  console.log(categories, randomDrinks)
   return (
-    <>
-    </>
+    <Box sx={{
+      display: "flex",
+      flexDirection: "column",
+    }}>
+      <Navbar />
+      {/* <HeroSection /> */}
+    </Box>
   )
 }
