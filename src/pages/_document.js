335da5e80c6094c9cd1d6cd278399e4e0a580e75
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-03-23 22:20:50 +0530

    font added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600;700&family=IBM+Plex+Sans:wght@100&family=Inknut+Antiqua:wght@300;400&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
