6ea15588aa54a5c14e9edb4b4da7eb5e663c0f78
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2023-03-23 20:39:25 +0530

    caraousel added

---
+++ src/components/HeroSection/Caraousel/BannerCard.jsx
@@ -0,0 +1,24 @@
+import Box from "@mui/material/Box";
+
+
+const BannerCard = ({ key, image }) => {
+    return (
+        <>
+            <Box
+                minWidth={"100%"}
+                width={"100%"}
+                px={{ lg: 3, md: 3, sm: 0, xs: 0 }}
+                pb={{ lg: 3, md: 3, sm: 1, xs: 1 }}
+                sx={{ cursor: "pointer" }}
+                onClick={() => {
+                    // if (entityType === 2) {
+                    //     window.open(entityId, "blank");
+                    // }
+                }}
+            >
+                <img src={image} width={"100%"} alt={"banner"} style={{ aspectRatio: '20/15' }} />
+            </Box>
+        </>
+    );
+};
+export default BannerCard;
--- src/components/HeroSection/Caraousel/index.jsx
+++ src/components/HeroSection/Caraousel/index.jsx
@@ -1,10 +1,53 @@
+import { Box } from "@mui/material";
+import BannerCard from "./BannerCard";
+import { Fab } from "@mui/material";
+import { useRef } from "react";
+import ArrowBackIosSharpIcon from "@mui/icons-material/ArrowBackIosSharp";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+
 const Caraousel = (props) => {
-    console.log(props)
+
+    const { randomDrinks } = props;
+    console.log("All drinks", randomDrinks[0].drinks[0].strDrinkThumb);
+
+    const refCard = useRef(null);
+    const scrollDiv = (scrollOffset, ref) => {
+        ref.current.scrollLeft += scrollOffset;
+    };
+
     return (
-        <>
-            This is caraousel
-        </>
+        <Box sx={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: { md: 'row', xs: 'column' }
+        }}>
+            <Box position={"relative"} width={{ md: '50vw', xs: '100vw' }}>
+                <Box
+                    display={"flex"}
+                    alignItems={"center"}
+                    ref={refCard}
+                    width={"100%"}
+                    sx={{
+                        scrollBehavior: "smooth",
+                        overflowX: "hidden",
+                    }}
+                >
+
+                    {randomDrinks.map((each) => (
+                        <BannerCard
+                            key={each.drinks[0].idDrink}
+                            image={each.drinks[0].strDrinkThumb}
+                        />
+                    ))}
+                </Box>
+            </Box>
+
+            <Box width={{ md: '50vw', xs: '100vw' }} bgcolor={'red'}>
+                hi
+            </Box>
+
+        </Box>
     )
 }
 
-export default Caraousel
+export default Caraousel;
